274c416e9ace7c82a3882d7e9e44df0af466e169
isaak111 <isak.lejon@utb.ecutbildning.se>
2022-02-04 13:48:36 +0100

    more classes

--- src/main/java/com/superdevs/VocalPlayer.java
+++ src/main/java/com/superdevs/VocalPlayer.java
@@ -1,13 +1,29 @@
 package com.superdevs;
 
-public class VocalPlayer implements MakeMove{
+import java.util.List;
+import java.util.Random;
+
+public class VocalPlayer implements MakeMove {
+
+    List<String> vokalWords = List.of("Det står att när de skulle göra pannkakor i fängelset använde de en fånge som smet",
+            "Var kan man köpa kor billigast i världen? Korea.",
+            "Vilken skillnad är det på en dator och en norrman? Datorn behöver bara instruktioner en gång.",
+            "Vet du vad det blir då man parar en pittbullterrier med en kommunalarbetare? En mördarsnigel!",
+            "Vakna Morfar! Du har glömt att ta dina sömntabletter!",
+            "Du måtte ha en livlig fantasi, Jonas, du ser så nöjd ut när du tittar dig i spegeln.",
+            "Vad heter Finlands sämste boxare? Ranta Runtiringen.");
+
     @Override
     public String makeMove() {
-        return null;
+        return "Move";
     }
 
     @Override
     public String punchLine() {
-        return null;
+        Random random = new Random();
+        int upperBound = vokalWords.size() - 1;
+        int intRand = random.nextInt(upperBound);
+        return vokalWords.get(intRand);
     }
+
 }
