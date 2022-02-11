65b25c0c03012e45232f1ee3344cbc796693e327
Korvin1337 <korvin1337@gmail.com>
2022-02-11 01:24:39 +0000

    added method with algorithm for tounrament results and statistics

--- src/main/java/com/superdevs/Player.java
+++ src/main/java/com/superdevs/Player.java
@@ -4,11 +4,31 @@ import java.util.Scanner;
 
 public class Player {
     String name;
-    int points;
+    int matchesPlayed;
+    int matchesWon;
+    int matchPoints;
+    int firstPlace;
+    int secondPlace;
+    int thirdPlace;
+    int fourthPlace;
+    int bestPlace;
+    int bestPlaceCount;
+    int worstPlace;
+    int worstPlaceCount;
 
     public Player(String name) {
         this.name = name;
-        this.points = 0;
+        this.matchesPlayed = 0;
+        this.matchesWon = 0;
+        this.matchPoints = 0;
+        this.firstPlace = 0;
+        this.secondPlace = 0;
+        this.thirdPlace = 0;
+        this.fourthPlace = 0;
+        this.bestPlace = 4;
+        this.bestPlaceCount = 0;
+        this.worstPlace = 0;
+        this.worstPlaceCount = 0;
     }
 
     public String makeMove(Player user) {
@@ -57,12 +77,92 @@ public class Player {
         this.name = name;
     }
 
-    public int getPoints() {
-        return points;
+    public int getMatchPoints() {
+        return matchPoints;
     }
 
-    public void setPoints(int points) {
-        this.points = points;
+    public int getFirstPlace() {
+        return firstPlace;
+    }
+
+    public int getSecondPlace() {
+        return secondPlace;
+    }
+
+    public int getThirdPlace() {
+        return thirdPlace;
+    }
+
+    public int getFourthPlace() {
+        return fourthPlace;
+    }
+
+    public int getMatchesPlayed() {
+        return matchesPlayed;
+    }
+
+    public int getBestPlace() {
+        return bestPlace;
+    }
+
+    public int getBestPlaceCount() {
+        return bestPlaceCount;
+    }
+
+    public int getWorstPlace() {
+        return worstPlace;
+    }
+
+    public int getWorstPlaceCount() {
+        return worstPlaceCount;
+    }
+
+    public int getMatchesWon() {
+        return matchesWon;
+    }
+
+    public void setMatchPoints(int matchPoints) {
+        this.matchPoints = matchPoints;
+    }
+
+    public void setFirstPlace(int firstPlace) {
+        this.firstPlace = firstPlace;
+    }
+
+    public void setSecondPlace(int secondPlace) {
+        this.secondPlace = secondPlace;
+    }
+
+    public void setThirdPlace(int thirdPlace) {
+        this.thirdPlace = thirdPlace;
+    }
+
+    public void setFourthPlace(int fourthPlace) {
+        this.fourthPlace = fourthPlace;
+    }
+
+    public void setMatchesPlayed(int matchesPlayed) {
+        this.matchesPlayed = matchesPlayed;
+    }
+
+    public void setBestPlace(int bestPlace) {
+        this.bestPlace = bestPlace;
+    }
+
+    public void setBestPlaceCount(int bestPlaceCount) {
+        this.bestPlaceCount = bestPlaceCount;
+    }
+
+    public void setWorstPlace(int worstPlace) {
+        this.worstPlace = worstPlace;
+    }
+
+    public void setWorstPlaceCount(int worstPlaceCount) {
+        this.worstPlaceCount = worstPlaceCount;
+    }
+
+    public void setMatchesWon(int matchesWon) {
+        this.matchesWon = matchesWon;
     }
 
     public static void pause(int milliseconds) {
@@ -78,8 +178,17 @@ public class Player {
     public String toString() {
         return "Player{" +
                 "name='" + name + '\'' +
-                ", points=" + points +
+                ", matchesPlayed=" + matchesPlayed +
+                ", matchesWon=" + matchesWon +
+                ", matchPoints=" + matchPoints +
+                ", firstPlace=" + firstPlace +
+                ", secondPlace=" + secondPlace +
+                ", thirdPlace=" + thirdPlace +
+                ", fourthPlace=" + fourthPlace +
+                ", bestPlace=" + bestPlace +
+                ", bestPlaceCount=" + bestPlaceCount +
+                ", worstPlace=" + worstPlace +
+                ", worstPlaceCount=" + worstPlaceCount +
                 '}';
     }
-
 }
